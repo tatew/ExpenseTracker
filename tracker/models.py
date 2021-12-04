9a09019293bd1832b206cb82442bbe2977b29b47
tatew <tatew@vt.edu>
2021-12-03 23:36:24 -0500

    Start adding auth

--- tracker/models.py
+++ tracker/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.db.models.deletion import PROTECT
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Method(models.Model):
+    name = models.CharField(max_length=100)
 
 class BalanceChange(models.Model):
     date = models.DateField()
     vendor = models.CharField(max_length=200)
     reason = models.CharField(max_length=500)
+    category = models.ForeignKey(Category, on_delete=PROTECT, null=True)
+    method = models.ForeignKey(Method, on_delete=PROTECT, null=True)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return HttpResponse("login failed")
+    else:
+        return render(request, 'tracker/login.html')
 
+@login_required
 def index(request):
     return HttpResponse("Hello, world. You're at the tracker index.")
